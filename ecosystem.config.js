bede6e98d65937487d8cebb07e520ee45313d59e
Kulikov Aleksey <k.alex.serg@gmail.com>
2025-02-27 22:02:19 +0100

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,15 @@ module.exports = {
 		name: 'sysadmin_chat_bot',
 		script: './index.mjs',
 		env: {
-			TOKEN: 'xxxxxx',
+			TOKEN:   '$TELEGRAMM_BOT_TOKEN',
 			DB_HOST: '',
 			DB_NAME: '',
 			DB_PASS: '',
-		    DB_PORT: 5432,
-		    DB_USER: ''
+		    	DB_PORT: 5432,
+		    	DB_USER: '',
+			DEEPSEEK_API_KEYL: '$DEEPSEEK_API_KEY'
 		},
-		watch: ['*.js'],
+		watch: ['*.js', '*.mjs'],
 		ignore_watch : ["node_modules", "logs"],
 		watch_delay: 1000
 	}]
